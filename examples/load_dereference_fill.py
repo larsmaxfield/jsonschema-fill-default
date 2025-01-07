9bdf03a10b8e9afcf95b6f83b3f1057b28954cd3
Lars Maxfield <maxfield.lars@gmail.com>
2025-01-07 16:05:47 +0100

    Change example to bicycle schema, instance
    From the integration test

---
+++ examples/load_dereference_fill.py
@@ -0,0 +1,41 @@
+import json
+from pathlib import Path
+
+from jsonschema import validate, Draft202012Validator
+from jsonref import replace_refs
+from jsonschema_fill_default import fill_default
+
+
+def main():
+    # Schema as JSON file, instance as Python dict
+    schema_filename = "bicycle.schema.json"
+    instance = {
+        "style": "road",
+        "color": "red"
+    }
+
+    # Load schema
+    schema_absolute_path = Path(__file__).parent / schema_filename
+    with open(schema_absolute_path, 'r') as file:
+        schema = json.load(file)
+
+    # Validate schema and validate instance against schema
+    Draft202012Validator.check_schema(schema)
+    validate(instance, schema)
+
+    # De-reference schema before using it to fill defaults
+    # jsonref.replace_refs replaces "$refs" in a schema with the referenced
+    # schemas themselves. Assigning its return immediately evaluates it and
+    # returns a deep copy.
+    schema = replace_refs(schema)
+
+    # Fill instance with schema defaults
+    # fill_default mutates the instance, so we don't assign its return
+    print(f"Original instance:\n{instance}\n")
+    fill_default(instance, schema)
+    validate(instance, schema)
+    print(f"Filled instance:\n{instance}\n")
+
+
+if __name__ == "__main__":
+    main()
