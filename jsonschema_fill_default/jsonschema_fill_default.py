e94c28569826562d05b1a31ad7037117f00114ee
Lars Maxfield <maxfield.lars@gmail.com>
2025-06-02 12:20:29 +0200

    Resolve #34: Add `FillConfig`, missing parent flag
    Add tests with create_missing_parent

--- jsonschema_fill_default/jsonschema_fill_default.py
+++ jsonschema_fill_default/jsonschema_fill_default.py
@@ -1,9 +1,26 @@
 from typing import Union
+from dataclasses import dataclass
 
 from jsonschema import validate, ValidationError
 
 
-def fill_default(instance: Union[dict, list], schema: dict) -> Union[dict, list]:
+@dataclass
+class FillConfig:
+    """Configuration for `fill_default` and its private methods
+
+    Args:
+        create_missing_parents (bool): If a parent is missing in the instance
+            and the schema has sub-defaults in the parent, then create that
+            parent in the instance and fill its sub-defaults.
+    """
+    create_missing_parents: bool = True
+
+
+def fill_default(
+        instance: Union[dict, list],
+        schema: dict,
+        config: Union[FillConfig, None] = None
+        ) -> Union[dict, list]:
     """Fill a JSON instance with schema defaults
 
     Recursively fills a JSON instance with the defaults of a schema with
@@ -24,32 +41,37 @@ def fill_default(instance: Union[dict, list], schema: dict) -> Union[dict, list]
     Args:
         instance (dict, list): JSON instance valid against the given schema
         schema (dict): JSON schema adhering to Draft 2020-12
+        config (FillConfig | None): Configuration for filling. If None, uses
+            default `FillConfig`.
 
     Returns:
         instance (dict, list): Mutated filled instance (not a copy).
     """
+    if config is None:
+        config = FillConfig()
     for keyword in schema:  # Apply keywords in order for predictable defaults
         if keyword == "properties":
-            _fill_properties(instance, schema)
+            _fill_properties(instance, schema, config)
         if keyword == "allOf":
-            _fill_allof(instance, schema)
+            _fill_allof(instance, schema, config)
         if keyword == "anyOf":
-            _fill_anyof(instance, schema)
+            _fill_anyof(instance, schema, config)
         if keyword == "if":
-            _fill_ifthenelse(instance, schema)
+            _fill_ifthenelse(instance, schema, config)
         if keyword == "oneOf":
-            _fill_oneof(instance, schema)
+            _fill_oneof(instance, schema, config)
         if keyword == "dependentSchemas":
-            _fill_dependentschemas(instance, schema)
+            _fill_dependentschemas(instance, schema, config)
         if keyword == "default":
             if not instance:
                 instance.update(schema["default"])
     if isinstance(instance, list):  # Handle "(prefix)Items" for lists (arrays)
-        _fill_prefixitems_and_items(instance, schema)
+        _fill_prefixitems_and_items(instance, schema, config)
     return None
 
 
-def _fill_prefixitems_and_items(instance: list, schema: dict):
+def _fill_prefixitems_and_items(
+        instance: list, schema: dict, config: FillConfig):
     """Recursively fill a list with schema "prefixItems" and "items" defaults
 
     Fills all nested structures.
@@ -80,7 +102,7 @@ def _fill_prefixitems_and_items(instance: list, schema: dict):
             # If an empty property filled with something from the schema
             # returns something, then it resolves to a default. If not, it has
             # no default.
-            if _fill_empty_property(prefixitem_schema) is not None:
+            if _fill_empty_property(prefixitem_schema, config) is not None:
                 n_schema_non_default_prefixitems -= 1
             else:
                 break
@@ -89,11 +111,12 @@ def _fill_prefixitems_and_items(instance: list, schema: dict):
     if n_instance_items > 0:  # Fill items
         if "items" in schema:
             for item in instance[-n_instance_items:]:
-                fill_default(item,  schema["items"])
+                fill_default(item,  schema["items"], config)
     elif n_instance >= n_schema_non_default_prefixitems:  # Fill missing prefixItems
         n_missing_prefixitems = len(schema["prefixItems"][n_instance:])
         for schema_of_missing_prefixitem in schema["prefixItems"][n_instance:]:
-            _property = _fill_empty_property(schema_of_missing_prefixitem)
+            _property = _fill_empty_property(
+                schema_of_missing_prefixitem, config)
             instance.append(_property)
 
     # For all existing prefixitems, fill default if dict or list
@@ -101,16 +124,16 @@ def _fill_prefixitems_and_items(instance: list, schema: dict):
     if n_existing_prefixitems > 0:
         for existing_instance, existing_schema in zip(instance[:n_existing_prefixitems], schema["prefixItems"][:n_existing_prefixitems]):  
             if isinstance(existing_instance, (dict, list)):
-                fill_default(existing_instance, existing_schema)
+                fill_default(existing_instance, existing_schema, config)
 
     return None
 
 
-def _fill_empty_property(schema: dict):
+def _fill_empty_property(schema: dict, config: FillConfig):
     """Return the default value of an empty property filled with a schema"""
     mock_schema = {"properties": {"property": schema}}
     mock_instance = {}
-    fill_default(mock_instance, mock_schema)
+    fill_default(mock_instance, mock_schema, config)
     if "property" in mock_instance:
         return mock_instance["property"]
     else:
@@ -124,7 +147,7 @@ def _is_empty_object(x):
     return not x
 
 
-def _fill_properties(instance: dict, schema: dict):
+def _fill_properties(instance: dict, schema: dict, config: FillConfig):
     """Recursively fill a JSON instance with schema "properties" defaults
 
     Fills all nested structures.
@@ -137,6 +160,7 @@ def _fill_properties(instance: dict, schema: dict):
         instance (dict): JSON instance valid against the given schema
         schema (dict): JSON schema adhering to Draft 2020-12 with a top-level
             "properties" keyword
+        config (FillConfig): Fill configuration
 
     Returns:
         None
@@ -144,12 +168,15 @@ def _fill_properties(instance: dict, schema: dict):
     for _property, subschema in schema["properties"].items():
         if any(key in ["properties", "oneOf", "allOf", "anyOf", "if", "dependentSchemas"] for key in subschema):  # Recursion
             if _property not in instance:
+                _was_missing = True
                 instance[_property] = dict()
                 _was_empty = False
             else:
+                _was_missing = False
                 _was_empty = _is_empty_object(instance[_property])
-            fill_default(instance[_property], subschema)
-            if not _was_empty and _is_empty_object(instance[_property]):
+            fill_default(instance[_property], subschema, config)
+            if (not _was_empty and _is_empty_object(instance[_property])) or \
+                    _was_missing and not config.create_missing_parents:
                 del instance[_property]
         if _property not in instance \
                 and "default" in subschema:
@@ -164,11 +191,11 @@ def _fill_properties(instance: dict, schema: dict):
                         subschema["default"][default_key]
         if "prefixItems" in subschema or "items" in subschema:
             if _property in instance:  # Instance must have array to fill
-                fill_default(instance[_property], subschema)
+                fill_default(instance[_property], subschema, config)
     return None
 
 
-def _fill_oneof(instance: dict, schema: dict):
+def _fill_oneof(instance: dict, schema: dict, config: FillConfig):
     """Recursively fill a JSON instance with schema "oneOf" defaults
 
     Fills all nested structures.
@@ -192,12 +219,12 @@ def _fill_oneof(instance: dict, schema: dict):
         except ValidationError:  # If not valid, go to next subschema
             i += 1
         else:  # If valid, fill with that subschema
-            fill_default(instance, subschema)
+            fill_default(instance, subschema, config)
             return None
     return None
 
 
-def _fill_allof(instance: dict, schema: dict):
+def _fill_allof(instance: dict, schema: dict, config: FillConfig):
     """Recursively fill a JSON instance with schema "allOf" defaults
 
     Fills all nested structures.
@@ -213,11 +240,11 @@ def _fill_allof(instance: dict, schema: dict):
         None
     """
     for subschema in schema["allOf"]:  # Instance is valid to all, so fill all
-        fill_default(instance, subschema)
+        fill_default(instance, subschema, config)
     return None
 
 
-def _fill_anyof(instance: dict, schema: dict):
+def _fill_anyof(instance: dict, schema: dict, config: FillConfig):
     """Recursively fill a JSON instance with schema "anyOf" defaults
 
     Fills all nested structures.
@@ -239,11 +266,11 @@ def _fill_anyof(instance: dict, schema: dict):
         except ValidationError:
             continue  # Skip to next subschema if instance is not valid to it
         else:
-            fill_default(instance, subschema)
+            fill_default(instance, subschema, config)
     return None
 
 
-def _fill_dependentschemas(instance: dict, schema: dict):
+def _fill_dependentschemas(instance: dict, schema: dict, config: FillConfig):
     """Recursively fill a JSON instance with schema "dependentSchemas" defaults
 
     Fills all nested structures.
@@ -260,13 +287,11 @@ def _fill_dependentschemas(instance: dict, schema: dict):
     """
     for _property, subschema in schema["dependentSchemas"].items():
         if _property in instance:
-            fill_default(instance, subschema)
+            fill_default(instance, subschema, config)
     return None
 
 
-
-
-def _fill_ifthenelse(instance: dict, schema: dict):
+def _fill_ifthenelse(instance: dict, schema: dict, config: FillConfig):
     """Recursively fill a JSON instance with schema "if-then(-else)" defaults
 
     Fills all nested structures.
@@ -285,7 +310,7 @@ def _fill_ifthenelse(instance: dict, schema: dict):
         validate(instance, schema["if"])
     except ValidationError:  # If invalid, fill instance with else if it exists
         if "else" in schema:
-            fill_default(instance, schema["else"])
+            fill_default(instance, schema["else"], config)
     else:
-        fill_default(instance, schema["then"])
+        fill_default(instance, schema["then"], config)
     return None
